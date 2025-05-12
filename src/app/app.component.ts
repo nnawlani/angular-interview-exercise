acf03822aaf3532ad0d693324d63be714455d147
System Administrator <root@Niteshs-MacBook-Air.local>
2025-05-12 05:29:43 -0700

    Added Component Defintion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { WeatherInformationService } from './weather-information.service';
 
 @Component({
     selector: 'app-root',
@@ -8,12 +10,50 @@ import { Observable } from 'rxjs';
     standalone: false
 })
 export class AppComponent implements OnInit {
-  constructor() {
+  stations: any[] = [];
+  selectedWeatherStation: string = '';
+  SelectedStationCurrentTemperature: any;
 
-  }
+  // Implementing Dependency Injection by injecting newly defined service
+  // instance to the Component via constructor injection
+  constructor(private WeatherService: WeatherInformationService) { }
 
   ngOnInit(): void {
-    
+    this.WeatherService.EnumerateWeatherStations().subscribe({
+      next: (data) => {
+        this.stations = data.features;
+      },
+      error: (err) => {
+        console.error('Failed to retrieve list of  weather stations:', err);
+      }
+   });
   }
 
+  // Event handler for handling the Weather Station selection event
+  onSelectingStation() {
+    this.WeatherService.GetCurrentWeatherInfo(this.selectedWeatherStation).subscribe({
+      next: (data) => {
+        this.SelectedStationCurrentTemperature = '';
+
+        // Checking for edge cases such as any of Feature collection or
+        // properties or temperature objects being empty
+        if(data.features[0] && data.features[0].properties && data.features[0].properties.temperature)
+        {
+           this.SelectedStationCurrentTemperature = data.features[0].properties.temperature;
+           if(this.SelectedStationCurrentTemperature.value)
+           {
+              const temperatureUnit = this.SelectedStationCurrentTemperature.unitCode;
+              this.SelectedStationCurrentTemperature = `Current Temperature: ${this.SelectedStationCurrentTemperature.value} ${temperatureUnit.substr(8)}`
+           } else {
+             this.SelectedStationCurrentTemperature = 'Current temperature not available for the selected station.'
+           }
+        } else {
+           this.SelectedStationCurrentTemperature = 'Current temperature not available for the selected station.'
+        }
+      },
+      error: (err) => {
+        console.error('Failed to retrieve current temperature:', err);
+      }
+    });
+  }
 }
