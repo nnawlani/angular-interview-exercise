786a2c1c247edb3408ab14b3d24b95bafd718aaf
System Administrator <root@Niteshs-MacBook-Air.local>
2025-05-12 05:51:10 -0700

    Updated root module to register newly added service to ensure its available throughout the Angular application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Import FormsModule to ensure ngModel directive can be recognized and processed
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherInformationService } from './weather-information.service';
 
+// Registering new Weather Information Service in the providers array
 @NgModule({ declarations: [
         AppComponent
     ],
     bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+        AppRoutingModule, FormsModule], providers: [provideHttpClient(withInterceptorsFromDi()), WeatherInformationService]
+})
 export class AppModule { }
